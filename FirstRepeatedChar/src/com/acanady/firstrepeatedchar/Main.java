761ac22370655fbad72df774e630707f8ec8e54b
acanady <alfonso.canadyjr@gmail.com>
2021-10-24 22:35:02 -0500

    Added solution for first repeated character

--- FirstRepeatedChar/src/com/acanady/firstrepeatedchar/Main.java
+++ FirstRepeatedChar/src/com/acanady/firstrepeatedchar/Main.java
@@ -1,9 +1,25 @@
 package com.acanady.firstrepeatedchar;
+import java.util.*;
 
 public class Main {
 
+
+    public static String firstrepeat(String s){
+        HashSet<String> characters = new HashSet<String>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(!characters.add(String.valueOf(s.charAt(i)))){
+                return String.valueOf(s.charAt(i));
+            }
+        }
+        return "-1";
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println(firstrepeat("character"));
+
     }
 
 }
+
+
