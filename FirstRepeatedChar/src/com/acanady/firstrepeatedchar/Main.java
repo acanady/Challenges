139202960d21c58bfb65dacf0ddaa22829764e8e
acanady <alfonso.canadyjr@gmail.com>
2021-10-24 22:49:46 -0500

    added firstbadversion main java class

--- FirstRepeatedChar/src/com/acanady/firstrepeatedchar/Main.java
+++ FirstRepeatedChar/src/com/acanady/firstrepeatedchar/Main.java
@@ -16,7 +16,7 @@ public class Main {
     }
 
     public static void main(String[] args){
-        System.out.println(firstrepeat("character"));
+        System.out.println(firstrepeat("Bologna"));
 
     }
 
