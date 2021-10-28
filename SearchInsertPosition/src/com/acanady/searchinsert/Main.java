a8f5a2cdeb828bb5b1e9b2849307944bc4960eab
acanady <alfonso.canadyjr@gmail.com>
2021-10-28 05:10:52 -0500

    added solution

--- SearchInsertPosition/src/com/acanady/searchinsert/Main.java
+++ SearchInsertPosition/src/com/acanady/searchinsert/Main.java
@@ -2,8 +2,47 @@ package com.acanady.searchinsert;
 
 public class Main {
 
+
+
+    public static int search(int[] nums, int number){
+        int min = 0;
+        int max = nums.length-1;
+        int pivot = max / 2;
+
+        if(nums.length == 0)
+            return min;
+
+        while(true){
+            if(min >= max){
+                if(nums[min] == number){
+                    System.out.println("number found");
+                    return min;
+                }
+                else{
+                    if(nums[min] > number)
+                        return min;
+                    else if (nums[min] < number)
+                        return min + 1;
+                }
+            }
+            if(nums[pivot] == number){
+                System.out.println("number found");
+                return pivot;
+            }
+            else if(nums[pivot] > number){
+                max = pivot-1;
+                pivot = (max - min) / 2 + min;
+            }
+            else{
+                min = pivot+1;
+                pivot = (max - min) / 2 + min;
+            }
+        }
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello world");
+        int[] nums = new int[]{};
+        System.out.println(search(nums, 30));
     }
 
 }
