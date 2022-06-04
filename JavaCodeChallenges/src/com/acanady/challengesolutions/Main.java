2b09fe0ed506bf369fb3c0eb7a5f26a074859972
acanady <alfonso.canadyjr@gmail.com>
2022-06-04 15:57:37 -0500

    added git ignore file for .cs solutions

--- JavaCodeChallenges/src/com/acanady/challengesolutions/Main.java
+++ JavaCodeChallenges/src/com/acanady/challengesolutions/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
     public static void main(String[] args){
 
-        IntersectionTwoArrays ita = new IntersectionTwoArrays();
-        System.out.println(Arrays.toString(ita.intersect()));
 
     }
 
